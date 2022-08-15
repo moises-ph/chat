387ee4a035a253a6755c8f42ea2c80f3f063f166
moisesph <mois.mp8@gmail.com>
2022-08-14 21:53:23 -0500

    Terminar

--- server/database.js
+++ server/database.js
@@ -1,5 +1,10 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://MoisesPH:jpctTumxjsMcwi76@myclustter.1zxpbln.mongodb.net/?retryWrites=true&w=majority";
+const dontenv = require('dotenv');
+
+const USER = process.env.MONGO_USER;
+const password = process.env.MONGO_PASSWORD;
+
+const uri = `mongodb+srv://${USER}:${password}@myclustter.1zxpbln.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 module.exports = client;
